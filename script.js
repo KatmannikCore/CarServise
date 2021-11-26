b5d2ec468857f251cf4eeb7d1591fe7acce81709
KatmannikCore <73733601+KatmannikCore@users.noreply.github.com>
2021-11-26 17:45:46 +0300

    Firsr relise

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+})
+
+
+
+
+$(document).ready(function() {
+    $('.sliderReviews').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        arrows: false,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
+})
